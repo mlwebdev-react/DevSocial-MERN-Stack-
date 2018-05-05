6eb183fe9296555a5d2954c93aea89c34ff19401
mlwebdev-react <mlwebdev-react@outlook.com>
2018-05-04 20:01:18 -0400

    M.E.R.N Stack social media site.
    DevSocial is a site for people in Web Development to communicate and share info on a site developed with the M.E.R.N Stack.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// Load User model
+const User = require("../../models/User");
+
 // @route GET api/users/test
 // @desc Test users route
 // @access Public
@@ -8,4 +11,25 @@ router.get("/test",
   (req, res) => res.json(
   { msg: "Users work" }));
 
+// @route  GET api/users/register
+// @desc   Register user
+// @access Public
+router.post("/register", 
+  (req, res) => { 
+    User.findOne({
+  email: req.body.email })
+  .then(user => {
+    if (user) {
+      return res.status(400).json({
+        email: "Email already exists"});
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password,
+      });
+    }
+  });
+});
 module.exports = router;
